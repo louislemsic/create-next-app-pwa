a40fb3b20f92e7d5895f4aff41a5ee15e7fd05ba
louislemsic <louislemsic@gmail.com>
2025-09-23 20:58:24 +0800

    Add Next.js configuration with service worker integration

---
+++ src/next.config.mjs
@@ -0,0 +1,11 @@
+import withSerwistInit from "@serwist/next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const withSerwist = withSerwistInit({
+    swSrc: "sw.ts",
+    swDest: "public/sw.js",
+});
+
+export default withSerwist(nextConfig);
